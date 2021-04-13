1e4b552423c143c783765c3986763a739ae23540
ox <agula002@ucr.edu>
2021-04-12 22:21:00 -0700

    got shufffle working

--- main.py
+++ main.py
@@ -11,15 +11,17 @@ class puzzle:
     def set_initial_state(self,state):
         self.initial_state = state
     def __init__(self,dim):
-        init_goal_state =np.arange(dim ** 2).reshape(dim,dim)
+        init_goal_state =np.arange(start=1,stop = dim ** 2+1)
         self.empty_tile =dim ** 2 
         self.goal_state = init_goal_state
-
+np.set_printoptions(linewidth=15)
 p1 = puzzle(4)
 
-print(np.random.shuffle(p1.goal_state))
+a = p1.goal_state.copy()
+np.random.shuffle(a)
+print(a)
 print(p1.empty_tile)
-print(p1.initial_state)
+#print(p1.initial_state)
 
 
 
