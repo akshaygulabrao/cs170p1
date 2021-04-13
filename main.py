22e7c74ba21bc94da03374ae05b1570d9ad69ff1
ox <agula002@ucr.edu>
2021-04-13 01:19:03 -0700

    made good progress on state class

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
-
 import numpy as np
-class state:
-    def __init__(self,arr,parent,dim):
-        self.arr = arr
-        self.parent = parent
-        self.goal = goal
+import anytree
 
+class state:
+    discovered_states = {} 
+    def swapLeft(self):
+        return 1 
+    def swapRight(self):
+        return 1
+    def swapUp(self):
+        return 2
+    def swapDown(self):
+        return 2 
+    def __init__(self,prev_operation_list,current_state):
+       self.prev_operation_list = prev_operation_list;
+       self.current_state = current_state
+       print(current_state.split())
 
 class puzzle:
     def set_custom_initial_state(self,state):
@@ -14,24 +23,18 @@ class puzzle:
         else:
             print('ILLEGAL BOARD')
             set_random_initial_state(self)
-        
     def set_random_initial_state(self):
-        self.inital_state = self.goal_state.copy()
         np.random.shuffle(self.initial_state)
+    def solve(self):
         print(self.initial_state)
+        state([],' '.join(map(str,self.initial_state)))
     def __init__(self,dim):
-        init_goal_state =np.arange(start=1,stop = dim ** 2+1)
-        self.empty_tile =dim ** 2 
-        self.goal_state = init_goal_state
-        linewidth_dict = { 3:7,4:15,5:17,6:19,7:22,8:25,9:28,10:44} 
+        self.goal_state = np.arange(start=1,stop = dim ** 2+1)
+        linewidth_dict = {2:5,3:7,4:15,5:17,6:19,7:22,8:25,9:28,10:44} 
         np.set_printoptions(linewidth=linewidth_dict[dim])
         self.initial_state = self.goal_state.copy() 
         self.set_random_initial_state()
-
-
-
-p1 = puzzle(3)
-
+p1 = puzzle(2)
 #print(p1.initial_state)
-
+p1.solve()
 
