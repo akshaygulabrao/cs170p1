62e91813ffff4472b93336c9f74f7d810eaf06dd
ox <agula002@ucr.edu>
2021-04-12 22:47:59 -0700

    line formatting

--- main.py
+++ main.py
@@ -8,20 +8,18 @@ class state:
 
 
 class puzzle:
-    def set_initial_state(self,state):
+    def set_custom_initial_state(self,state):
         self.initial_state = state
+    def set_random_initial_state(self,state):
+        self.initial_state = np.random.shuffle(p1.goal_state.copy())
     def __init__(self,dim):
         init_goal_state =np.arange(start=1,stop = dim ** 2+1)
         self.empty_tile =dim ** 2 
         self.goal_state = init_goal_state
-np.set_printoptions(linewidth=15)
-p1 = puzzle(4)
+        linewidth_dict = { 4:15,5:17,6:19,7:22,8:25,9:28,10:44} 
+        np.set_printoptions(linewidth=linewidth_dict[dim])
+p1 = puzzle(10)
 
-a = p1.goal_state.copy()
-np.random.shuffle(a)
-print(a)
-print(p1.empty_tile)
-#print(p1.initial_state)
-
+print(p1.goal_state)
 
 
