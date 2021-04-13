fb3988930148b96b7138c98c596c5d04f5598dd1
ox <agula002@ucr.edu>
2021-04-12 23:24:26 -0700

    finished initial shuffling

--- main.py
+++ main.py
@@ -9,17 +9,29 @@ class state:
 
 class puzzle:
     def set_custom_initial_state(self,state):
-        self.initial_state = state
-    def set_random_initial_state(self,state):
-        self.initial_state = np.random.shuffle(p1.goal_state.copy())
+        if len(state) == dim ** 2:
+            self.initial_state = state
+        else:
+            print('ILLEGAL BOARD')
+            set_random_initial_state(self)
+        
+    def set_random_initial_state(self):
+        self.inital_state = self.goal_state.copy()
+        np.random.shuffle(self.initial_state)
+        print(self.initial_state)
     def __init__(self,dim):
         init_goal_state =np.arange(start=1,stop = dim ** 2+1)
         self.empty_tile =dim ** 2 
         self.goal_state = init_goal_state
-        linewidth_dict = { 4:15,5:17,6:19,7:22,8:25,9:28,10:44} 
+        linewidth_dict = { 3:7,4:15,5:17,6:19,7:22,8:25,9:28,10:44} 
         np.set_printoptions(linewidth=linewidth_dict[dim])
-p1 = puzzle(10)
+        self.initial_state = self.goal_state.copy() 
+        self.set_random_initial_state()
 
-print(p1.goal_state)
+
+
+p1 = puzzle(3)
+
+#print(p1.initial_state)
 
 
