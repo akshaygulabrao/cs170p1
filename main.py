00fcee227d2a6efb1a6a4c23f2ce1ed04a82d9db
ox <agula002@ucr.edu>
2021-04-14 16:05:48 -0700

    working on changing to the new frontier version

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 import numpy as np
 import anytree
 import math
+import queue
+
+explored_set= set()
+frontier = queue.Queue() 
+
+def stringToArray(string):
+    return np.asarray(list(map(int,string.split())))
+def arrayToString(nparray):
+    return ' '.join(map(str,nparray)) 
+
 
 class state:
-    discovered_states =set()
-
     def swapLeft(self):
         if self.marker % self.dim != 0:
             self.current_state_np[self.marker],self.current_state_np[self.marker-1]=self.current_state_np[self.marker-1],self.current_state_np[self.marker] 
@@ -42,27 +50,13 @@ class state:
             self.current_state_np[self.marker],self.current_state_np[self.marker + self.dim]= self.current_state_np[self.marker + self.dim],self.current_state_np[self.marker]
             string_state = self.arrayToString(self.current_state_np)
             self.prevMovelist.append((string_state,'Swap Down'))
-            if string_state in self.discovered_states:
-                return 0
-            else:
-                self.discovered_states.add(string_state)
-                state(self.prevMovelist,string_state)
-                return 1
+        return self.current_state_np
     def eval(self):
         job_list = []
         job_list.append(self.swapDown())
         job_list.append(self.swapUp())
-        job_list.append(self.swapLeft())
-        job_list.append(self.swapRight())
         return [job for job in job_list if job != 0]
 
-    def stringToArray(self,string):
-        return np.asarray(list(map(int,string.split())))
-    def arrayToString(self,nparray):
-        return ' '.join(map(str,nparray)) 
-    def checkGoalState(self):
-        #checks if the list is sorted
-        return np.all(self.current_state_np[:-1] <= self.current_state_np[1:])
 
     def __init__(self,prevMovelist,current_state_string):
        self.prevMovelist = prevMovelist;
@@ -77,7 +71,7 @@ class state:
 
 class puzzle:
     discovered_states = set()
-    frontier = []
+    frontier = queue.Queue() 
     def set_custom_initial_state(self,state):
         if len(state) == self.dim ** 2:
             self.initial_state = state
@@ -86,11 +80,18 @@ class puzzle:
             set_random_initial_state(self)
     def set_random_initial_state(self):
         np.random.shuffle(self.initial_state)
-    def solve(self,):
+    def checkGoalState(self):
+        #checks if the list is sorted
+        return np.all(self.current_state_np[:-1] <= self.current_state_np[1:])
+    def solve(self):
         print(self.initial_state)
-        frontier.push(state([],' '.join(map(str,self.initial_state))))
-        while frontier not empty:
-            frontier.push(frontier.pop().eval())
+        frontier.put('',arrayToString(self.initial_state))
+        explored_set = set()
+        while(1):
+            if frontier.empty():
+                return 'failure'
+             
+        
     def __init__(self,dim):
         self.dim = dim
         self.goal_state = np.arange(start=1,stop = dim ** 2+1)
@@ -98,8 +99,5 @@ class puzzle:
         np.set_printoptions(linewidth=linewidth_dict[dim])
         self.initial_state = self.goal_state.copy() 
         self.set_random_initial_state()
-p1 = puzzle(10)
-#print(p1.initial_state)
-#p1.set_custom_initial_state(np.asarray(list(map(int,'1 2 4 3'.split()))))
+p1 = puzzle(2)
 p1.solve()
-
