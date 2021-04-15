6825cb0df503047aea3b2050c8e1ca7c78e404a9
ox <agula002@ucr.edu>
2021-04-14 20:59:09 -0700

    current progress

--- main.py
+++ main.py
@@ -15,8 +15,9 @@ def arrayToString(nparray):
 class state:
     def swapLeft(self):
         if self.marker % self.dim != 0:
-            self.current_state_np[self.marker],self.current_state_np[self.marker-1]=self.current_state_np[self.marker-1],self.current_state_np[self.marker] 
-            string_state = self.arrayToString(self.current_state_np)
+            arr = stringToArray(current_state_string)
+            arr[self.marker],arr[self.marker-1] = arr[self.marker -1],arr[self.marker]
+            string_state = self.arrayToString(arr)
             self.prevMovelist.append((string_state,'Swap Right'))
             if string_state in self.discovered_states:
                 return 0
@@ -46,24 +47,26 @@ class state:
                 self.discovered_states.add(string_state)
                 return state(self.prevMovelist,string_state)
     def swapDown(self):
-        if self.marker + self.dim < self.dim ** 2 - 1:
-            self.current_state_np[self.marker],self.current_state_np[self.marker + self.dim]= self.current_state_np[self.marker + self.dim],self.current_state_np[self.marker]
-            string_state = self.arrayToString(self.current_state_np)
-            self.prevMovelist.append((string_state,'Swap Down'))
-        return self.current_state_np
+        if self.marker + self.dim <= self.dim ** 2 - 1:
+            print('SwapDown')
+            arr = stringToArray(self.current_state_string)
+            arr[self.marker],arr[self.marker+self.dim]= arr[self.marker+self.dim],arr[self.marker]
+            string_state = arrayToString(arr)
+            app_str = self.prevMovelist + 'Swap Down ' 
+            print(arrayToString(arr))
+            new_state = (app_str,arrayToString(arr))
+            return new_state
     def eval(self):
         job_list = []
         job_list.append(self.swapDown())
-        job_list.append(self.swapUp())
-        return [job for job in job_list if job != 0]
+        print(job_list)
+        return job_list 
 
 
     def __init__(self,prevMovelist,current_state_string):
        self.prevMovelist = prevMovelist;
        self.current_state_string = current_state_string
-       print(self.current_state_string)
-       self.discovered_states.add(current_state_string)
-       self.current_state_np = self.stringToArray(current_state_string)
+       self.current_state_np = stringToArray(current_state_string)
        self.marker = np.argmax(self.current_state_np)
        self.dim = int(math.sqrt(self.current_state_np[self.marker]))
        
@@ -72,12 +75,13 @@ class state:
 class puzzle:
     discovered_states = set()
     frontier = queue.Queue() 
-    def set_custom_initial_state(self,state):
-        if len(state) == self.dim ** 2:
-            self.initial_state = state
+    def set_initial_state(self,state):
+        arr = stringToArray(state)
+        if len(arr) == self.dim ** 2:
+            self.initial_state =arr 
         else:
             print('ILLEGAL BOARD')
-            set_random_initial_state(self)
+            self.set_random_initial_state()
     def set_random_initial_state(self):
         np.random.shuffle(self.initial_state)
     def checkGoalState(self):
@@ -85,12 +89,17 @@ class puzzle:
         return np.all(self.current_state_np[:-1] <= self.current_state_np[1:])
     def solve(self):
         print(self.initial_state)
-        frontier.put('',arrayToString(self.initial_state))
+        frontier.put(('',arrayToString(self.initial_state)))
         explored_set = set()
-        while(1):
-            if frontier.empty():
-                return 'failure'
-             
+        if frontier.empty():
+            return 'failure'
+        else:
+            chosen_state = frontier.get() 
+            #print(chosen_state)
+            s = state(chosen_state[0],chosen_state[1])
+            #print(s.eval())
+            #print(s.prevMovelist,s.current_state_string)
+              
         
     def __init__(self,dim):
         self.dim = dim
@@ -100,4 +109,5 @@ class puzzle:
         self.initial_state = self.goal_state.copy() 
         self.set_random_initial_state()
 p1 = puzzle(2)
+p1.set_initial_state('1 4 3 2')
 p1.solve()
